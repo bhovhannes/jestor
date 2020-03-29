6ecae257a7a71381b34417a89a5927e2d03495f7
Hovhannes Babayan <bhovhannes@gmail.com>
2020-03-29 16:44:00 +0400

    chore: upgrade deps

--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,9 @@ interface IJestor {
 export function jestor(jestMock: jest.Mock): IJestor {
   const rules = []
   return {
-    followRules: ruleGetter => {
+    followRules: (ruleGetter) => {
       ruleGetter({
-        whenCalledWith: getRuleBuilder(rule => rules.push(rule))
+        whenCalledWith: getRuleBuilder((rule) => rules.push(rule)),
       })
       return jestMock.mockImplementation((...actualArgs) => {
         for (let i = rules.length - 1; i >= 0; --i) {
@@ -29,7 +29,7 @@ export function jestor(jestMock: jest.Mock): IJestor {
         }
       })
     },
-    whenCalledWith: getRuleBuilder(rule => {
+    whenCalledWith: getRuleBuilder((rule) => {
       rules.length = 0
       return jestMock.mockImplementation((...actualArgs) => {
         const result = rule.matcher(actualArgs)
@@ -37,25 +37,26 @@ export function jestor(jestMock: jest.Mock): IJestor {
           return rule.valueWrapper(rule.returnValue)
         }
       })
-    })
+    }),
   }
 }
 
-const equals = (function() {
+const equals = (function () {
   let jestEqual = (a: any, b: any) => {}
   try {
     const MATCHER_NAME = '__matcherToGetEquals'
     expect.extend({
-      [MATCHER_NAME]: function() {
+      [MATCHER_NAME]: function () {
         jestEqual = this.equals
         return {
           pass: true,
-          message: () => ''
+          message: () => '',
         }
-      }
+      },
     })
     expect(1)[MATCHER_NAME]()
     delete expect[MATCHER_NAME]
+    // @ts-ignore  TS2304: Cannot find name 'global'.
     delete global[Symbol.for('$$jest-matchers-object')].matchers[MATCHER_NAME]
   } catch (e) {
     // eslint-disable-next-line no-empty
@@ -64,7 +65,7 @@ const equals = (function() {
 })()
 
 function getRuleMatcher(args) {
-  return actualArgs =>
+  return (actualArgs) =>
     actualArgs.length === args.length &&
     actualArgs.every((actualArg, i) => {
       const arg = args[i]
@@ -79,34 +80,34 @@ function getRuleBuilder(collectRule) {
     const rule = {
       matcher: getRuleMatcher(expectedArgs),
       valueWrapper: undefined,
-      returnValue: undefined
+      returnValue: undefined,
     }
     return {
-      return: returnValue => {
-        rule.valueWrapper = v => v
+      return: (returnValue) => {
+        rule.valueWrapper = (v) => v
         rule.returnValue = returnValue
         collectRule(rule)
       },
 
-      resolveWith: value => {
-        rule.valueWrapper = v => Promise.resolve(v)
+      resolveWith: (value) => {
+        rule.valueWrapper = (v) => Promise.resolve(v)
         rule.returnValue = value
         collectRule(rule)
       },
 
-      rejectWith: value => {
-        rule.valueWrapper = v => Promise.reject(v)
+      rejectWith: (value) => {
+        rule.valueWrapper = (v) => Promise.reject(v)
         rule.returnValue = value
         collectRule(rule)
       },
 
-      throw: value => {
-        rule.valueWrapper = v => {
+      throw: (value) => {
+        rule.valueWrapper = (v) => {
           throw v
         }
         rule.returnValue = value
         collectRule(rule)
-      }
+      },
     }
   }
 }
