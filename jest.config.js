4eabf6e6c5148c686b487b079066537e27fa1e06
Hovhannes Babayan <bhovhannes@gmail.com>
2021-02-20 23:46:06 +0400

    chore: switch to npx

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,8 @@ module.exports = {
   globals: {
     'ts-jest': {
       diagnostics: {
-        ignoreCodes: ['TS151001']
-      }
-    }
-  }
+        ignoreCodes: ['TS151001'],
+      },
+    },
+  },
 }
